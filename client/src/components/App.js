9e76a2937f92d9442efea4945c278cf864715008
Kieran McManus <kmcmanus41092@gmail.com>
2019-02-19 23:04:05 -0500

    Migrated old project over and started updates for improvement

---
+++ client/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Search from './Search';
+import Artist from './Artist';
+import Tracks from './Tracks';
+import reactlogo from '../assets/reactlogo.png';
+import spotify from '../assets/spotify.png';
+
+const API_ADDRESS = 'http://localhost:3000';
+
+class App extends Component {
+  state = { artist: null, tracks: [] };
+
+  searchArtist = artistQuery => {
+    fetch(`${API_ADDRESS}/artist/${artistQuery}`)
+      .then(response => response.json())
+      .then(json => {
+        if (json.artists.total > 0) {
+          const artist = json.artists.items[0];
+
+          this.setState({ artist });
+
+          fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => this.setState({ tracks: json.tracks }))
+            .catch(error => alert(error.message));
+        }
+      })
+      .catch(error => alert(error.message));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App-title">
+          <img className="react-logo" src="../assets/reactlogo.png" alt="react-logo"/>  with
+          <img className ="spotify-logo" src="../spotify.png" alt ="spotify-logo"/>
+        </div>
+        <Search searchArtist={this.searchArtist} />
+        <Artist artist={this.state.artist} />
+        <Tracks tracks={this.state.tracks} />
+      </div>
+    );
+  }
+}
+
+export default App;
